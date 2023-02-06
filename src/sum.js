14da6a7cd2e18c793713e1e84464afc004befa6a
Jonald Damaso <damasojonald@gmail.com>
2023-02-06 16:11:22 +0000

    refactor to ES6 approached

--- src/sum.js
+++ src/sum.js
@@ -1,8 +1,9 @@
 function sum(...numbers) {
-    let count = 0;
-    for(let number of numbers){
-        count += number;
-    }
-    return count;
+    // let count = 0;
+    // for(let number of numbers){
+    //     count += number;
+    // }
+    // return count;
+    return numbers.reduce((sum, n) => sum + n, 0);
   }
   module.exports = sum;
